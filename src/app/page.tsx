42faf450cdaea7f9ed0a1cdbb2155f545bc44f91
klimisnotavailable <klimgrigor28@gmail.com>
2025-06-23 19:47:47 +0300

    feat/about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HomeHero from "@/components/sections/HomeHero/HomeHero";
+import AboutSection from "@/components/sections/AboutSection/AboutSection";
 export default function Home() {
   return (
     <div>
       <main>
         <HomeHero></HomeHero>
+        <AboutSection></AboutSection>
       </main>
     </div>
   );
