e91e947a7e6af2573e065c22ff64c9828cc8a117
klimisnotavailable <klimgrigor28@gmail.com>
2025-08-18 19:36:08 +0300

    klim/mobile
    blog,article pages

--- src/components/sections/SimilarArticles/SimilarArticles.tsx
+++ src/components/sections/SimilarArticles/SimilarArticles.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { useEffect, useState } from "react";
 import NewsItem from "@/components/NewsItem/NewsItem";
+import { NewItem } from "../NewsSection/NewsSection";
 import Container from "@/components/Container/Container";
 import Link from "next/link";
-import { NewItem } from "../NewsSection/NewsSection";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import s from "./SimilarArticles.module.css";
 
 export default function SimilarArticles({ category }: { category?: number }) {
   const [postsData, setPostsData] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -25,6 +28,7 @@ export default function SimilarArticles({ category }: { category?: number }) {
 
     fetchPosts();
   }, [category]);
+
   return (
     <section className={s.section}>
       <Container>
@@ -32,14 +36,69 @@ export default function SimilarArticles({ category }: { category?: number }) {
           <h2>Схожі статті</h2>
           <Link href={"/blog"}>До всіх статтей {arrow}</Link>
         </div>
-        {postsData ? (
+        {postsData && window.innerWidth >= 1024 ? (
           <ul className={`${s.newsList}`}>
             {postsData.slice(0, 3).map((item: NewItem) => {
               return <NewsItem item={item} key={item.id} />;
             })}
           </ul>
         ) : (
-          <p>please wait</p>
+          ""
+        )}
+        {window.innerWidth <= 1024 && postsData ? (
+          <>
+            <Swiper
+              modules={[Navigation]}
+              navigation={{
+                nextEl: `.${s.swiperNext}`,
+                prevEl: `.${s.swiperPrev}`,
+                disabledClass: s.disabled,
+              }}
+              slidesPerView={"auto"}
+              onSlideChange={(s) => {
+                setActiveIndex(s.activeIndex);
+              }}
+              onSwiper={(s) => {
+                setActiveIndex(s.activeIndex);
+              }}
+              className={s.swiper}
+            >
+              {postsData.map((item: NewItem) => {
+                return (
+                  <SwiperSlide key={item.id}>
+                    <NewsItem item={item} />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+            <div className={s.mobileNav}>
+              <div
+                className={`${s.swiperPrev} ${s.navBtn} ${
+                  activeIndex === 0 ? s.disabled : ""
+                }`}
+              >
+                {arrow}
+              </div>
+              <div className={s.mobPagination}>
+                <p className={s.activeSlide}>
+                  {activeIndex ? activeIndex + 1 : 1}
+                </p>
+                <p>/{postsData.length}</p>
+              </div>
+              <div
+                className={`${s.swiperNext} ${s.navBtn} ${
+                  activeIndex === postsData.length - 1 ? s.disabled : ""
+                }`}
+              >
+                {arrow}
+              </div>
+            </div>
+            <Link className={s.blogLink} href={"/blog"}>
+              До всіх статтей {arrow}
+            </Link>
+          </>
+        ) : (
+          ""
         )}
       </Container>
     </section>
@@ -54,6 +113,6 @@ const arrow = (
     viewBox="0 0 14 10"
     fill="none"
   >
-    <path d="M0.5 5H12.5M12.5 5L8.26471 1M12.5 5L8.26471 9" stroke="white" />
+    <path d="M0.5 5H12.5M12.5 5L8.26471 1M12.5 5L8.26471 9" />
   </svg>
 );
